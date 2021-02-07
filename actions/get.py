af238b65a2cf727a5edfa24826014e335a49a50b
Shawn <62177488+lattisimo@users.noreply.github.com>
2021-02-06 23:03:24 -0600

    Simplify actions (#1)
    * full remodel. simplified actions
    
    * remove data if empty
    
    * fix track_task action name
    
    * fix entrypoint
    
    * .execute typo
    
    * tweeking for requrements
    
    * more base actions
    
    * adjust name
    
    * well that broke it
    
    * description tweaking
    
    * change exec name to execute
    
    * comment updates
    
    * add secret flag
    
    * re-add defaults
    
    Co-authored-by: Shawn Robinson <shawn.robinson@sagenet.com>

---
+++ actions/get.py
@@ -0,0 +1,60 @@
+"""Device Manager Database Get module"""
+from lib.actions import BaseFortiManagerAction
+from pyFMG.fortimgr import (FMGBaseException,
+                            FMGValidSessionException,
+                            FMGConnectTimeout,
+                            FMGConnectionError)
+
+
+__all__ = [
+    'Get'
+]
+
+
+class Get(BaseFortiManagerAction):
+    """Default get action class"""
+
+    def run(self, **kwargs):
+        """
+        Default get action method.
+
+        :host: overwrite fortimanager ip or hostname
+        :username: overwriting username
+        :password: overwriging password
+        :url: api endpoint
+        :data: dictionary of data to pass as arguments
+        :fmg_debug: print rpc calls to stdout
+        :return: (boolean, result)
+        """
+        fortimanager = kwargs.pop("host")
+        username = kwargs.pop("username")
+        password = kwargs.pop("password")
+        url = kwargs.pop("url")
+        data = kwargs.pop("data")
+        fmg_debug = kwargs.pop("fmg_debug")
+        try:
+            with self.fmgconnector(fortimanager,
+                                   username=username,
+                                   password=password,
+                                   debug=fmg_debug
+                                   ) as instance:
+                self.logger.info("{}".format(str(instance)))
+                self.logger.info("{}".format("FortiManager instance endpoint {}".format(url)))
+                # if data:
+                code, result = instance.get(url, **data)
+                # else:
+                #     code, result = instance.get(url)
+            self.logger.info("{}".format("FortiManager instance disonnected"))
+
+            if code:
+                return (False, result)
+            return (True, result)
+
+        except FMGValidSessionException:
+            self.logger.critical("Invalid Session - Check Credentials")
+            return (False, "Session Failed")
+        except (FMGConnectTimeout, FMGConnectionError):
+            self.logger.critical("Unreachable Host - Check Hostname/IP")
+        except FMGBaseException:
+            self.logger.exception("Connection Error")
+            return (False, "Connection Failed")
---
+++ actions/set.py
@@ -0,0 +1,57 @@
+"""Device Manager Database Set module"""
+from lib.actions import BaseFortiManagerAction
+from pyFMG.fortimgr import (FMGBaseException,
+                            FMGValidSessionException,
+                            FMGConnectTimeout,
+                            FMGConnectionError)
+
+
+__all__ = [
+    'Set'
+]
+
+
+class Set(BaseFortiManagerAction):
+    """Default set action class"""
+
+    def run(self, **kwargs):
+        """
+        Default set action method.
+
+        :host: overwrite fortimanager ip or hostname
+        :username: overwriting username
+        :password: overwriging password
+        :url: api endpoint
+        :data: dictionary of data to pass as arguments
+        :fmg_debug: print rpc calls to stdout
+        :return: (boolean, result)
+        """
+        fortimanager = kwargs.pop("host")
+        username = kwargs.pop("username")
+        password = kwargs.pop("password")
+        url = kwargs.pop("url")
+        data = kwargs.pop("data")
+        fmg_debug = kwargs.pop("fmg_debug")
+        try:
+            with self.fmgconnector(fortimanager,
+                                   username=username,
+                                   password=password,
+                                   debug=fmg_debug
+                                   ) as instance:
+                self.logger.info("{}".format(str(instance)))
+                self.logger.info("{}".format("FortiManager instance endpoint {}".format(url)))
+                code, result = instance.set(url, **data)
+            self.logger.info("{}".format("FortiManager instance disonnected"))
+
+            if code:
+                return (False, result)
+            return (True, result)
+
+        except FMGValidSessionException:
+            self.logger.critical("Invalid Session - Check Credentials")
+            return (False, "Session Failed")
+        except (FMGConnectTimeout, FMGConnectionError):
+            self.logger.critical("Unreachable Host - Check Hostname/IP")
+        except FMGBaseException:
+            self.logger.exception("Connection Error")
+            return (False, "Connection Failed")
